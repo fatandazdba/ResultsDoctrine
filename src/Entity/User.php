5cfbd0b3854632185eaf6824d5ef04bfd226e3f3
Freddy Tandazo <freddy.tandazo.yanez@alumnos.upm.es>
2018-12-04 19:01:06 +0100

    database implementada

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -144,6 +144,56 @@ class User implements \JsonSerializable
     }
 
     /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isEnabled(): bool
+    {
+        return $this->enabled;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isAdmin(): bool
+    {
+        return $this->isAdmin;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    /**
      * Set password
      *
      * @param string $password password
